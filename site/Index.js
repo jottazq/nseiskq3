a6812dbf854d29029e618820abb598357da4089a
jotta <154549754+jottazq@users.noreply.github.com>
2024-12-29 01:13:19 -0300

    Atualizar o Index.js

--- site/Index.js
+++ site/Index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 999o;
+const PORT = 9999;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
